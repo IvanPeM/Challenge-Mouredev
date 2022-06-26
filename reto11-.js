5502f804b3d08c88bb38148ae3898ca1095d52cb
IvanPe <ivan.pe.martinez@gmail.com>
2022-06-27 00:39:32 +0200

    Commit 9

--- reto11-.js
+++ reto11-.js
@@ -7,9 +7,12 @@
  * Fecha publicación resolución: 21/03/22
  * Dificultad: FÁCIL
  *
- * Enunciado: Crea una función que reciba dos cadenas como parámetro (str1, str2) e imprima otras dos cadenas como salida (out1, out2).
- * - out1 contendrá todos los caracteres presentes en la str1 pero NO estén presentes en str2.
- * - out2 contendrá todos los caracteres presentes en la str2 pero NO estén presentes en str1.
+ * Enunciado: Crea una función que reciba dos cadenas como parámetro 
+ * (str1, str2) e imprima otras dos cadenas como salida (out1, out2).
+ * - out1 contendrá todos los caracteres presentes en la str1 pero NO 
+ * estén presentes en str2.
+ * - out2 contendrá todos los caracteres presentes en la str2 pero NO 
+ * estén presentes en str1.
  *
  * @param {Stirng} tex1 
  * @param {String} tex2 
@@ -23,6 +26,12 @@ function eliminarCaracteres(tex1,tex2){
     console.log("out2 : ",out2);
 }
 
+/**
+ * Funcion que convierte una frase en una lista
+ * de las palabras de la frase.
+ * @param {String} tex 
+ * @returns {[*]}
+ */
 function textoALista(tex){
     let palabras = [];
     let pal = "";
@@ -37,6 +46,13 @@ function textoALista(tex){
     return palabras;
 }
 
+/**
+ * Funcion que comprueba y te indica las palabras
+ * que no contienen.
+ * @param {[*]} pa 
+ * @param {[*]} pal 
+ * @returns {String}
+ */
 function out(pa,pal){
     let ou = "";
     for(let p of pa){
