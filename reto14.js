28bca60bf97dc2fde16ab4f6fb7030b2b43a891d
IvanPeM <ivan.pe.martinez@gmail.com>
2022-08-07 15:03:22 +0200

    Commit 11

---
+++ reto14.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/**
+ * Reto #14
+ * ¿ES UN NÚMERO DE ARMSTRONG?
+ * Fecha publicación enunciado: 04/04/22
+ * Fecha publicación resolución: 11/04/22
+ * Dificultad: FÁCIL
+ *
+ * Enunciado: Escribe una función que calcule si un número dado es un número de Armstrong (o también llamado narcisista).
+ * Si no conoces qué es un número de Armstrong, debes buscar información al respecto.
+ * 
+ * @param {Number} n 
+ * @returns {Boolean}
+ */
+function armstrong(n){
+    let boo = false;
+    return boo;
+}
